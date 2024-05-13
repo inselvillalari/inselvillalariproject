7e4c25a52564c46640480a9888b3a3b82547a901
Goksu Celik <dev.goksucelik@gmail.com>
2024-05-13 21:29:11 +0300

    first

--- src/pages/home1/index.jsx
+++ src/pages/home1/index.jsx
@@ -14,6 +14,7 @@ import Contact from "../../components/Contact";
 const Home1 = () => {
   React.useEffect(() => {
     document.querySelector("body").classList.add("homepage");
+    
   }, []);
   return (
     <LightLayout footerClass={"mt-30"}>
