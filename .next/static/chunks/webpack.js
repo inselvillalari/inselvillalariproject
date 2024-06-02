a8d3998ba530bc3273edd8e2f94324e2387ec0b4
Goksu Celik <dev.goksucelik@gmail.com>
2024-06-02 14:11:35 +0300

    templatecommits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "828e13ad193ebc52"; }
+/******/ 		__webpack_require__.h = function() { return "269e376e211705cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
