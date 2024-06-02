d3ca748aa6143a189f3fcf7a7676224e86765c45
Goksu Celik <dev.goksucelik@gmail.com>
2024-06-02 17:53:57 +0300

    fix

---
+++ src/pages/gredi/activities/index.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import MainLayout from "../../../layouts/main";
+import PageHeader from "../../../components/Page-header";
+import ProjectIntro from "./ProjectIntro";
+import ProjectVideo from "../../../components/Project-Video";
+
+const GrediActivities = () => {
+  React.useEffect(() => {
+    document.querySelector("body").classList.add("index3");
+  }, []);
+  return (
+    <MainLayout>
+      <PageHeader
+        title="VİLLA GREDİ"
+        fullPath={[
+          { id: 1, name: "Anasayfa", url: "/" },
+          { id: 2, name: "İletişim", url: "/contact" },
+        ]}
+        image="/assets/img/portfolio/project1/bg.jpg"
+      />
+      <ProjectIntro />
+      <section className="projdtal">
+        <div className="justified-gallery">
+          <div className="row">
+            <a href="#" className="col-lg-4 col-xl-3 col-md-12">
+              <img alt="" src="/assets/img/portfolio/project1/1.jpg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+              <img alt="" src="/assets/img/portfolio/project1/2.jpg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+              <img alt="" src="/assets/img/portfolio/project1/6.jpg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-12">
+              <img alt="" src="/assets/img/portfolio/project1/3.jpg" />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <ProjectVideo />
+
+      <section className="projdtal">
+        <div className="justified-gallery">
+          <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+            <img alt="" src="/assets/img/portfolio/project1/8.jpg" />
+          </a>
+          <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+            <img alt="" src="/assets/img/portfolio/project1/9.jpg" />
+          </a>
+        </div>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default GrediActivities;
