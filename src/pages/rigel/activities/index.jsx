5eb67c57ecf8fb2d5b410091c1fe275f047298cf
Goksu Celik <dev.goksucelik@gmail.com>
2024-06-02 19:43:12 +0300

    fotogrflar

---
+++ src/pages/rigel/activities/index.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import MainLayout from "../../layout/MainLayout";
+import PageHeader from "../../../components/Page-header";
+import ProjectIntro from "./ProjectIntro";
+import ProjectVideo from "../../../components/Project-Video";
+
+const RigelActivities = () => {
+  React.useEffect(() => {
+    document.querySelector("body").classList.add("index3");
+  }, []);
+  return (
+    <MainLayout>
+      <PageHeader
+        title="VİLLA RİGEL"
+        fullPath={[
+          { id: 1, name: "Anasayfa", url: "/" },
+          { id: 2, name: "İletişim", url: "/contact" },
+        ]}
+        image="/assets/img/portfolio/project1/bg.jpg"
+      />
+      <ProjectIntro />
+      <section className="projdtal">
+        <div className="justified-gallery">
+          <div className="row">
+            <a href="#" className="col-lg-4 col-xl-3 col-md-12">
+              <img alt="" src="/assets/img/slid/rigel/activities/3.jpeg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+              <img alt="" src="/assets/img/slid/rigel/activities/4.jpeg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+              <img alt="" src="/assets/img/slid/rigel/activities/1.jpeg" />
+            </a>
+            <a href="#" className="col-lg-4 col-xl-3 col-md-12">
+              <img alt="" src="/assets/img/slid/rigel/activities/5.jpeg" />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <ProjectVideo />
+
+      <section className="projdtal">
+        <div className="justified-gallery">
+          <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+            <img alt="" src="/assets/img/slid/rigel/activities/6.jpeg" />
+          </a>
+          <a href="#" className="col-lg-4 col-xl-3 col-md-6">
+            <img alt="" src="/assets/img/slid/rigel/activities/7.jpeg" />
+          </a>
+        </div>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default RigelActivities;
