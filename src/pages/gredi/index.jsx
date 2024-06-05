9617c155fbaf5af99edb2800b76271562406ab10
Goksu Celik <info@inselvillalari.com>
2024-06-05 11:31:47 +0300

    text fix

--- src/pages/gredi/index.jsx
+++ src/pages/gredi/index.jsx
@@ -16,7 +16,7 @@ const Gredi = () => {
           // second: "and the building speak for themeselves",
         }}
         // title = "text"
-        content="Kalkan Kızıltaş'ta yer alan, çok özel bir lokasyonda, mavi ve yeşille bütünleşmiş, kendini gösteren, modern ve çarpıcı bir etki yaratan lüks bir villadır. Konumundan dolayı, Kalkan manzarasını ayaklarınızın altına sermektedir. Villamızın giriş katında deniz manzaralı Amerikan mutfak ve salon, üst katlarında beş adet suit yatak odası bulunmakta olup, ebeveyn odalarının birinde yer alan jakuzide manzaraya karşı keyif de yapabileceksiniz. Villamız, bütün bir yılın yorgunluğunu üzerinizden atmanız için kusursuz bir şekilde tasarlanmıştır. Siz değerli misafirlerimize 'işte bu' dedirtecek bir tatil geçirtmek için; kapılarımızı açıyoruz…"
+        content="Kalkan Kızıltaş'ta yer alan, çok özel bir lokasyonda, mavi ve yeşille bütünleşmiş, kendini gösteren, modern ve çarpıcı bir etki yaratan lüks bir villadır. Villamız, bütün bir yılın yorgunluğunu üzerinizden atmanız için kusursuz bir şekilde tasarlanmıştır. Siz değerli misafirlerimize 'işte bu' dedirtecek bir tatil geçirtmek için; kapılarımızı açıyoruz…"
       />
       <GrediPage />
     </MainLayout>
--- src/pages/rigel/index.jsx
+++ src/pages/rigel/index.jsx
@@ -16,9 +16,7 @@ const Rigel = () => {
           // second: "and the building speak for themeselves",
         }}
         // title = "text"
-        content="Kalkan Kızıltaş mevkiinde yer alan, çok özel bir lokasyonda, mavi ve yeşille bütünleşmiş, kendini gösteren, modern ve çarpıcı bir etki yaratan lüks bir villadır. Konumundan dolayı, Kalkan manzarasını ayaklarınızın altına sermektedir. Villamızın giriş katında deniz manzaralı Amerikan mutfak - salon üst katlarında ise 5 adet suit yatak odası bulunmakta olup ebeveyn odalarının ikisinde yer alan jakuzide, manzaraya karşı keyif de yapabileceksiniz. Bodrum katta spor salonu (bilardo, masa tenisi, koşu bandı, kondisyon bisikleti) ve ısıtmalı kapalı havuz ile bütün bir yılın yorgunluğunu üzerinizden atmanız için, kusursuz bir şekilde tasarlanmıştır. Siz değerli misafirlerimize 'işte bu' dedirtecek bir tatil geçirtmek için kapılarımızı açıyoruz…
-
-"
+        content="Kalkan Kızıltaş'ta yer alan, çok özel bir lokasyonda, mavi ve yeşille bütünleşmiş, kendini gösteren, modern ve çarpıcı bir etki yaratan lüks bir villadır. Villamız, bütün bir yılın yorgunluğunu üzerinizden atmanız için kusursuz bir şekilde tasarlanmıştır. Siz değerli misafirlerimize 'işte bu' dedirtecek bir tatil geçirtmek için; kapılarımızı açıyoruz…"
       />
       <RigelPage />
     </MainLayout>
