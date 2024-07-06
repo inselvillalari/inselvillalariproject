787453267397d6183bd46118c672c7930405d378
Goksu Celik <dev.goksucelik@gmail.com>
2024-07-06 14:15:05 +0300

    link

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -15,13 +15,13 @@ const Portfolio2 = () => {
               <h1 className="custom-font">Patara</h1>
             </div>
             <div className="more">
-              <a
+              <Link
                 href="https://tr.wikipedia.org/wiki/Patara"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 Detaya Git <i className="fas fa-chevron-right"></i>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="col-lg-2 col-md-6 cluom" data-tab="tab-2">
@@ -29,13 +29,13 @@ const Portfolio2 = () => {
               <h1 className="custom-font">Kaş</h1>
             </div>
             <div className="more">
-              <a
+              <Link
                 href="https://www.google.com/search?q=ka%C5%9F%27ta+gezilecek+yerler&sca_esv=c8c65b53b193be60&biw=2560&bih=1294&ei=7blcZp3KG93ixc8Ps-SumAw&udm=&oq=ka%C5%9Fta+ge&gs_lp=Egxnd3Mtd2l6LXNlcnAiCWthxZ90YSBnZSoCCAAyDRAAGIAEGLEDGIMBGAoyBxAAGIAEGAoyBxAAGIAEGAoyBxAAGIAEGAoyCBAAGBYYChgeMgYQABgWGB4yBhAAGBYYHjIIEAAYFhgKGB4yCBAAGBYYChgeMggQABgWGB4YD0iKFlDJBli4DHAAeAKQAQCYAZIBoAGnBaoBAzAuNbgBA8gBAPgBAZgCBqACtgXCAgQQABhHwgIMEAAYgAQYQxiKBRgKwgIFEAAYgATCAggQABiABBiiBJgDAIgGAZAGCJIHAzEuNaAHkSQ&sclient=gws-wiz-serp"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 Detaya Git <i className="fas fa-chevron-right"></i>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="col-lg-2 col-md-6 cluom" data-tab="tab-3">
@@ -43,13 +43,13 @@ const Portfolio2 = () => {
               <h1 className="custom-font">Kaputaş Plajı</h1>
             </div>
             <div className="more">
-              <a
+              <Link
                 href="https://www.google.com/search?q=Kaputa%C5%9F+plaj%C4%B1&oq=Kaputa%C5%9F+plaj%C4%B1&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDU0MTVqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 Detaya Git <i className="fas fa-chevron-right"></i>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="col-lg-2 col-md-6 cluom" data-tab="tab-4">
@@ -57,13 +57,13 @@ const Portfolio2 = () => {
               <h1 className="custom-font">Saklıkent</h1>
             </div>
             <div className="more">
-              <a
+              <Link
                 href="https://tr.wikipedia.org/wiki/Sakl%C4%B1kent_Mill%C3%AE_Park%C4%B1"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 Detaya Git <i className="fas fa-chevron-right"></i>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="col-lg-2 col-md-6 cluom" data-tab="tab-5">
@@ -85,13 +85,13 @@ const Portfolio2 = () => {
               <h1 className="custom-font">Tlos</h1>
             </div>
             <div className="more">
-              <a
+              <Link
                 href="https://tr.wikipedia.org/wiki/Tlos"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 Detaya Git <i className="fas fa-chevron-right"></i>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
