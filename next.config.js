5e9b3156fc6b069e4b5b648a0b2a38ceb28c44e6
Goksu Celik <dev.goksucelik@gmail.com>
2024-07-06 15:52:16 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  productionBrowserSourceMaps: true,
+  productionBrowserSourceMaps: false,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "css")],
