fcf1b900524674eb63d79190bf10467d5c67a553
dev-tolgakaya <dev.tolgakaya@gmail.com>
2024-11-07 23:21:14 +0300

    aktivite

--- src/gredi/GrediPage.jsx
+++ src/gredi/GrediPage.jsx
@@ -87,37 +87,42 @@ const GrediPage = () => {
                 </div>
               </div>
             </Link>
-            {/* <Link href="/gredi/insideViews">
+            <Link href="/gredi/insideViews">
               <div className="col-lg-6 items interior">
                 <div className="item">
                   <div className="img">
-                    <img alt="" src="/assets/img/slid/gredi/indoor/2.jpeg" />
+                    <img
+                      alt=""
+                      src="/assets/img/slid/gredi/indoor/2.jpeg"
+                      className="gallery-img-class"
+                    />
                   </div>
                   <div className="cont vis">
                     <h5>İç Mekan</h5>
-                    <span>Architecture</span>
-                    <span>Modern</span>
+                    {/* <span>Architecture</span>
+                    <span>Modern</span> */}
                   </div>
                 </div>
               </div>
-            </Link> */}
-            {/* <Link href="/gredi/activities">
+            </Link>
+            <Link href="/gredi/activities">
               <div className="col-lg-6 items residential">
                 <div className="item">
                   <div className="img">
                     <img
                       alt=""
                       src="/assets/img/slid/gredi/activities/2.jpeg"
+                      className="gallery-img-class"
                     />
                   </div>
                   <div className="cont vis">
                     <h5>Aktiviteler</h5>
-                    <span>Architecture</span>
-                    <span>Modern</span>
+                    {/* <span>Architecture</span>
+                    <span>Modern</span> */}
                   </div>
                 </div>
               </div>
-            </Link> */}
+            </Link>
           </div>
         </div>
         {/* <BookingCalendar dateRangesData={dateRangesData} /> */}
