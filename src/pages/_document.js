ac6259d310ae26f50b030680ff3e7d86197d6a75
goksucelik <dev.goksucelik@gmail.comdev>
2025-05-06 16:21:11 +0300

    css fix deneme 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,20 +10,29 @@ class MyDocument extends Document {
     return (
       <Html lang="tr">
         <Head>
-          {/* Temel Meta */}
+          {/* Preconnect for Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+
+          {/* Fontlar - optimize edilmiş css2 sürümü */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100..900&family=Teko:wght@300..700&family=Playfair+Display:wght@400..900&family=Cinzel:wght@400..900&family=Jost:wght@100..900&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* Meta Bilgiler */}
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="robots" content="index, follow" />
           <meta name="author" content="Sevgül Göksu Çelik, Tolga Kaya" />
-
-          {/* SEO Anahtar Kelimeler */}
           <meta
             name="keywords"
             content="inselvillalari, insel, ilba, ilkin berberoğlu, selçuk ince, ilkinberberoglu, selcukince, kalkan, kaş, kiralık villa, lüks villa"
           />
-
-          {/* Açıklama */}
           <meta
             name="description"
             content="İnsel Villaları, Kalkan ve Kaş'ta özel havuzlu, deniz manzaralı lüks kiralık villalar sunar. Unutulmaz bir tatil deneyimi yaşayın."
@@ -46,7 +54,7 @@ class MyDocument extends Document {
             content="https://inselvillalari.com/assets/img/slid/villasCoverImg/agena.jpeg"
           />
 
-          {/* Twitter Card (gerekirse) */}
+          {/* Twitter Card */}
           <meta name="twitter:card" content="summary_large_image" />
           <meta
             name="twitter:title"
@@ -66,42 +74,10 @@ class MyDocument extends Document {
 
           {/* Favicon */}
           <link rel="shortcut icon" href="/assets/img/favicon.ico" />
-
-          {/*<!-- 1. Preconnect -->*/}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-
-          {/* Fontlar */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Teko:300,400,500,600,700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Playfair+Display:400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
-
         <body>
           <Main />
           <NextScript />
-          <SpeedInsights />
         </body>
       </Html>
     );
