fd415ddd431feb576f8a3677fb3ebdf3dae8ff7c
goksucelik <dev.goksucelik@gmail.comdev>
2025-05-23 00:00:55 +0300

    fotograf boyutlari duzeltildi

--- src/pages/capella/dayViews/index.jsx
+++ src/pages/capella/dayViews/index.jsx
@@ -11,7 +11,7 @@ const CapellaDayViews = () => {
   const [toggler, setToggler] = useState(false);
   const [slideIndex, setSlideIndex] = useState(1);
 
-  const imageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const imageNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
   const sources = imageNumbers.map(
     (num) => `/assets/img/slid/capella/dayViews/${num}.jpeg`
