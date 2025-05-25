db19b6ec905b6a2c351345c134302df0b35678ee
dev-tolgakaya <dev.tolgakaya@gmail.com>
2025-05-25 10:48:59 +0300

    login rev

--- src/pages/admin/login.jsx
+++ src/pages/admin/login.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { useRouter } from "next/router";
 import { adminLogin } from "../../store/auth/thunk";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import withLoading from "../../common/withLoading";
 import { useTranslation } from "react-i18next";
+import { Button, Grid, TextField } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function AdminLoginPage() {
   const { t } = useTranslation("common");
@@ -20,34 +23,53 @@ function AdminLoginPage() {
     dispatch(adminLogin({ username, password, router }));
   };
 
+  const { loading } = useSelector((state) => state.auth);
+
   return (
     <div style={{ maxWidth: 400, margin: "100px auto", padding: 20 }}>
       <h2>Admin Giriş</h2>
       <form onSubmit={handleLogin}>
         <div style={{ marginBottom: 12 }}>
-          <label>Kullanıcı Adı:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={{ width: "100%", padding: 8 }}
-            required
-          />
-        </div>
-        <div style={{ marginBottom: 12 }}>
-          <label>Şifre:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{ width: "100%", padding: 8 }}
-            required
-          />
+          <Grid item xs={12} sm={6} md={3} p={2}>
+            <TextField
+              fullWidth
+              autoComplete="off"
+              size="small"
+              id="username"
+              name="username"
+              label="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3} p={2}>
+            <TextField
+              fullWidth
+              size="small"
+              autoComplete="off"
+              id="password"
+              name="password"
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              type="password"
+            />
+          </Grid>
         </div>
         {error && <div style={{ color: "red", marginBottom: 10 }}>{error}</div>}
-        <button type="submit" style={{ padding: 10, width: "100%" }}>
-          Giriş Yap
-        </button>
+        <Grid item xs={12} sm={6} md={3} px={2} py={1}>
+          <LoadingButton
+            variant="contained"
+            type="submit"
+            color="primary"
+            fullWidth
+            loading={loading}
+          >
+            Giriş Yap
+          </LoadingButton>
+        </Grid>
       </form>
     </div>
   );
