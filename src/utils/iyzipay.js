e910d397dd4d595c0622f06bc154f547b8f87266
dev-tolgakaya <dev.tolgakaya@gmail.com>
2025-05-31 16:04:48 +0300

    prod uri

--- src/utils/iyzipay.js
+++ src/utils/iyzipay.js
@@ -4,7 +4,7 @@ import Iyzipay from "iyzipay";
 const iyzipay = new Iyzipay({
   apiKey: process.env.IYZICO_API_KEY,
   secretKey: process.env.IYZICO_SECRET_KEY,
-  uri: "https://sandbox-api.iyzipay.com" // PROD: https://api.iyzipay.com
+  uri: "https://api.iyzipay.com",
 });
 
 export default iyzipay;
